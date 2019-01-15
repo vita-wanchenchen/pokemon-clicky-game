07b2e4de4dcbe336258801ddde1b91a10a729fd5
Vita Chen <39967407+vita-wanchenchen@users.noreply.github.com>
2019-01-15 18:28:01 -0500

    Updates

---
+++ precache-manifest.fcc71d478b310d3d84ebe35797ef9d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f28de29b1637082ee4b7",
+    "url": "/pokemon-clicky-game/static/js/runtime~main.f28de29b.js"
+  },
+  {
+    "revision": "0c64fc8d4043d7361f52",
+    "url": "/pokemon-clicky-game/static/js/main.0c64fc8d.chunk.js"
+  },
+  {
+    "revision": "8d0e6daf5a4074e9364a",
+    "url": "/pokemon-clicky-game/static/js/1.8d0e6daf.chunk.js"
+  },
+  {
+    "revision": "0c64fc8d4043d7361f52",
+    "url": "/pokemon-clicky-game/static/css/main.2c239c61.chunk.css"
+  },
+  {
+    "revision": "fad3c80822370d0dcb9eecaba8e50364",
+    "url": "/pokemon-clicky-game/index.html"
+  }
+];
